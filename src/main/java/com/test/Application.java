cb3327aa90e13db15538a664f0a7b16ed8e67782
Victor Szoltysek <victorszoltysek@victors-mbp.huntington.local>
2016-11-03 15:11:44 -0400

    Adding WebServer + Spring Boot + Templating.

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -1,10 +1,20 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.io.FileReader;
 
+@SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    public void testEngine() throws Exception{
         ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
         ScriptEngine engine = scriptEngineManager.getEngineByName("nashorn");
 
